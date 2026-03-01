dfa7df042d90c92082618be78f6221d89ad611de
ShrutikaChavan22 <shrutikachavan2211@gmail.com>
2026-03-01 23:07:34 +0530

    added built in function

---
+++ FunctionsAndPointer/builtinfunction.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int sq_root=sqrt(16);
+    printf("Square root of 16:%d \n",sq_root);
+    int cube_root=sqrt(8);
+    printf("cube root of 8:%d \n",cube_root);
+    int a=2,b=3,p;
+    p=pow(a,b);
+    printf("2 raised to power 3 is :%d\n",p);
+    return 0;
+}
