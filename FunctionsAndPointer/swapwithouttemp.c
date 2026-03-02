04b46b02d980c6c597869170a83830659563dfd8
ShrutikaChavan22 <shrutikachavan2211@gmail.com>
2026-03-02 15:30:42 +0530

    swap two number without using temp variable

---
+++ FunctionsAndPointer/swapwithouttemp.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+int swap(int a,int b){
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    printf("After swapping value of a and b:");
+    printf("%d , %d",a,b);
+}
+int main(){
+    int a,b;
+    printf("Enter value of a:");
+    scanf("%d",&a);
+    printf("Enter value of b:");
+    scanf("%d",&b);
+    swap(a,b);
+    return 0;
+}
